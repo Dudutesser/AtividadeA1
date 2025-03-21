c678d6aa02086edf938675c41f2ab37389e0c30d
Dudutesser <luisbertolitesser@gmail.com>
2025-03-21 12:08:45 -0400

    Questao01

--- src/Questoes/Questao01.java
+++ src/Questoes/Questao01.java
@@ -1,5 +1,26 @@
 package Questoes;
 
+import java.util.Scanner;
+
 public class Questao01 {
-    
+    public static void main(String[] args) {
+        Scanner num1 = new Scanner(System.in);
+        Scanner num2 = new Scanner(System.in);
+        Integer n1;
+        Integer n2;
+        System.out.println("Digite o primeiro numero: ");
+        n1 = num1.nextInt();
+        n2 = num2.nextInt();
+        System.out.println("O maior número é: " + funcao(n1, n2));
+        num1.close();
+        num2.close();
+    }
+
+    static Integer funcao(Integer x, Integer y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
 }
